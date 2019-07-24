2dd70af47ab9943c1222ef5fd55bfef0aa99d4e4
Mike Upton <uptonm@wit.edu>
2019-07-24 13:03:20 -0400

    Updating to default user location when unfetchable

--- client-v2/src/components/GeoLocated.js
+++ client-v2/src/components/GeoLocated.js
@@ -11,14 +11,14 @@ class GeoLocated extends React.Component {
         Your browser does not support Geolocation
       </h1>
     ) : !this.props.isGeolocationEnabled ? (
-      <h1 className="loading-text">Geolocation is not enabled</h1>
+      <SearchBar lat={42.352917} long={-71.055687} />
     ) : this.props.coords ? (
       <SearchBar
         lat={this.props.coords.latitude}
         long={this.props.coords.longitude}
       />
     ) : (
-      <h1 className="loading-text">Loading...</h1>
+      <SearchBar lat={42.352917} long={-71.055687} />
     );
   }
 }
