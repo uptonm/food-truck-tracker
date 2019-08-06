6021e8a07a844fb5edcf93a548a2679790a47c3c
Mike Upton <uptonm@wit.edu>
2019-08-06 18:07:17 -0400

    Initial commit

--- server/routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-// This class handles the routing for searching within a radius, and getting a particular trucks information
-const express = require('express');
-const actions = require('../controllers/searchLogic');
-const router = express.Router();
-
-router.get('/search', actions.search);
-router.get('/search/:id', actions.searchOne);
-
-module.exports = router;
